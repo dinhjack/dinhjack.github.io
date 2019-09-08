d865951b5205fb1da963c940ab78a9b7e13e5d0a
DKHT <thevien01@gmail.com>
2019-09-08 20:13:13 +0700

    init commit

--- processing_request.php
+++ processing_request.php
@@ -30,40 +30,199 @@
 				<h1>Followers Generator</h1>
 			</div>
 		</div>
-		<div class="content-wrapper generator-content-wrapper">					
-			<div class="processing-first-step">	
-				<div class="row">	
-					<form id="followers-form" class="followers-form first col-sm-12" role="form" >
-						<div class="form-group ig-username-form-group">	
-							<label>Enter your IG Username</label>
-							<div class="shake-wrapper">
-							<div class="ig-username-wrapper">		
-								<img src="img/add-username-icon.png" class="ig-username-input-icon" />
-								<input id="username" name="username" type="text" class="ig-username-input" placeholder="Profile Username..." required data-error="Please enter your username."/>
+		<div class="content-wrapper generator-content-wrapper">				
+			<div class="row">
+				<div class="profile-form-wrapper col-sm-12">
+					<?php									
+						$username = $_GET['username'];
+						$url = "https://www.instagram.com/" . $username;									
+						$data = @file_get_contents($url);
+						preg_match('/\"followed_by\"\:\s?\{\"count\"\:\s?([0-9]+)/',$data,$followed_by);
+						preg_match('/\"follows\"\:\s?\{\"count\"\:\s?([0-9]+)/',$data,$following);
+						preg_match('/\"full_name"\:\s?\"([a-zA-Z ]+)/',$data,$full_name);
+						preg_match('/\"profile_pic_url"\:\s?\"((http[s]?:\/\/)?([^\/\s]+\/)(.*?)")/',$data,$profile_pic);					
+					if ($data === false) { ?>
+						<div class="processing-wrapper-not-connected">
+							<div class="processing-inner-wrapper">
+								<div class="cssload-dots">
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+								</div>
+								<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+									<defs>
+										<filter id="goo">
+											<feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="12" ></feGaussianBlur>
+											<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0	0 1 0 0 0	0 0 1 0 0	0 0 0 18 -7" result="goo" ></feColorMatrix>
+											<!--<feBlend in2="goo" in="SourceGraphic" result="mix" ></feBlend>-->
+										</filter>
+									</defs>
+								</svg>
+								<p class="animated infinite jello">Retrieving Profile Info...</p>
 							</div>
+						</div>
+						<div class="error-message">
+							<i class="fa fa-times-circle-o" aria-hidden="true"></i>
+							<h1>Oops, error</h1>
+							<div class="error-message-notice">It seems you entered an invalid Username which we can't find!</div>
+							<div class="error-button-wrapper"><a id="error-go-back-button" class="error-button-back ig-button small">Go Back</a></div>
+						</div>							
+					<?php }
+					else { ?>
+						<div class="processing-wrapper">
+							<div class="processing-inner-wrapper">
+								<div class="cssload-dots">
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+									<div class="cssload-dot"></div>
+								</div>
+								<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+									<defs>
+										<filter id="goo">
+											<feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="12" ></feGaussianBlur>
+											<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0	0 1 0 0 0	0 0 1 0 0	0 0 0 18 -7" result="goo" ></feColorMatrix>
+											<!--<feBlend in2="goo" in="SourceGraphic" result="mix" ></feBlend>-->
+										</filter>
+									</defs>
+								</svg>
+								<p class="animated infinite jello">Retrieving Profile Info...</p>
 							</div>
-							<div class="help-block with-errors"></div>
-						</div>					
-						<div class="form-group submit-form-group">	
-							<button type="submit" id="generator-submit" class="followers-form-submit ig-button animated infinite jello" >Get Free Followers</button>
 						</div>
-					</form>				
-				</div>	
-				<div class="instructions-outer-container">
-					<div class="row">
-						<div class="example-img-wrapper col-sm-4">
-							<div class="example-img-overlay-wrapper"><a id="username-example-img-link" href="img/username-example.jpg"><img class="img-responsive" alt="IG Username Example" src="img/username-example.jpg"/><span class="example-img-overlay">Username Example</span></a></div>						
+						<div class="processing-second-step">
+							<div class="profile-info-wrapper">
+								<h3>Selected Profile Info</h3>
+								<div class="profile-info-content-wrapper">
+									<div class="row profile-info-row">
+										<div class="profile-img-wrapper col-xs-3">
+											<div class="flip-container">
+												<div class="card">
+													<img class="profile-picture-img back img-responsive" alt="Profile Picture Image" src="<?php if (!empty($profile_pic)) { echo $profile_pic[1]; } ?>"/>
+													<img class="profile-picture-img front img-responsive" alt="Profile Picture Image" src="<?php if (!empty($profile_pic)) { echo $profile_pic[1]; } ?>"/>			
+												</div>
+											</div>									
+										</div>
+										<div class="profile-info-field-username col-xs-9">
+											<div class="profile-info-username"><?php if (!empty($full_name)) { echo $full_name[1]; } ?> • (@<span id="console-username-val"><?php if (!empty($username)) { echo $username; } ?></span>)</div>
+											<span id="current-followers-value" class="profile-info-content"><?php if (!empty($followed_by)) { echo $followed_by[1]; } ?></span>
+											<span class="profile-info-label">followers</span><span class="profile-info-separator">•</span>						
+											<span class="profile-info-content"><?php if (!empty($following)) { echo $following[1]; } ?></span>
+											<span class="profile-info-label">following</span>
+										</div>
+									</div>
+									<div class="go-back-wrapper">Wrong profile? <span id="go-back">Go back</span></div>
+								</div>
+							</div>
+							<div class="amount-of-followers-selection-wrapper">
+								<h3>Select the amount of followers</h3>
+								<div class="row amount-of-followers-selection-row">	
+									<div class="amount-of-followers-selection-item-wrapper col-xs-4 amount-of-followers-selection-item-wrapper-1">	
+										<div class="amount-of-followers-selection-item amount-of-followers-selection-item-1">	
+											<img class="followers-amount-selection-img img-responsive" alt="1000 Followers" src="img/followers-selection-1.png"/>		
+											<div class="amount-of-followers-selection-item-value"><span>1000</span>Followers</div>
+										</div>
+									</div>
+									<div class="amount-of-followers-selection-item-wrapper col-xs-4 amount-of-followers-selection-item-wrapper-2">	
+										<div class="amount-of-followers-selection-item amount-of-followers-selection-item-2">	
+											<img class="followers-amount-selection-img img-responsive" alt="2500 Followers" src="img/followers-selection-2.png"/>		
+											<div class="amount-of-followers-selection-item-value"><span>2500</span>Followers</div>
+										</div>
+									</div>
+									<div class="amount-of-followers-selection-item-wrapper col-xs-4 amount-of-followers-selection-item-wrapper-3">	
+										<div class="amount-of-followers-selection-item amount-of-followers-selection-item-3">	
+											<img class="followers-amount-selection-img img-responsive" alt="5000 Followers" src="img/followers-selection-3.png"/>		
+											<div class="amount-of-followers-selection-item-value"><span>5000</span>Followers</div>
+										</div>
+									</div>
+								</div>
+								<button type="submit" id="add-followers-button" class="add-followers-button ig-button" >Add Followers</button>
+							</div>
+							<div id="adding-followers-console-notice-wrapper" class="adding-followers-console-notice-wrapper mfp-hide">
+								<div class="adding-followers-console-notice-inner-wrapper">
+									<div class="adding-followers-console-notice-header">
+										<img class="console-notice-logo img-responsive" alt="IG Logo Small" src="img/logo-dark-small.png" />
+										<h4>Followers Generator</h4>
+									</div>
+									<div class="adding-followers-console-notice-content">
+										<p class="console-notice-info-line">You are about to add <strong><span id="console-notice-followers-value"></span> followers</strong> to Instagram account <strong><span id="console-notice-account">@<?php if (!empty($username)) { echo $username; } ?></span></strong></p>
+										<p>This will increase the amount of Followers to <strong><span id="console-notice-followers-new-value"></span></strong></p>
+									</div>
+									<div class="console-notice-button-wrapper">
+										<button type="submit" id="generate-followers-button" class="generate-followers-button ig-button" >Sure, go ahead!</button>
+										<a id="console-notice-go-back">No, I changed my mind</a>
+									</div>
+								</div>
+							</div>
+							<div class="adding-followers-console-animation-wrapper">
+								<div class="adding-followers-console-animation">
+									<div class="cssload-dots-2">
+										<div class="cssload-dot"></div>
+										<div class="cssload-dot"></div>
+										<div class="cssload-dot"></div>
+										<div class="cssload-dot"></div>
+										<div class="cssload-dot"></div>
+									</div>
+									<svg version="1.1" xmlns="http://www.w3.org/2000/svg">
+										<defs>
+											<filter id="goo2">
+												<feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="12" ></feGaussianBlur>
+												<feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0	0 1 0 0 0	0 0 1 0 0	0 0 0 18 -7" result="goo" ></feColorMatrix>
+												<!--<feBlend in2="goo" in="SourceGraphic" result="mix" ></feBlend>-->
+											</filter>
+										</defs>
+									</svg>
+								</div>
+							</div>
+							<div class="adding-followers-console-wrapper">
+								<div class="adding-followers-console-row">	
+									<div class="adding-followers-console-inner-wrapper">											
+										<div class="adding-followers-console">	
+											<h4>Generator Console</h4>
+											<div class="adding-followers-console-content">
+												<div id="fountainG">
+													<div id="fountainG_1" class="fountainG"></div>
+													<div id="fountainG_2" class="fountainG"></div>
+													<div id="fountainG_3" class="fountainG"></div>
+													<div id="fountainG_4" class="fountainG"></div>
+													<div id="fountainG_5" class="fountainG"></div>
+													<div id="fountainG_6" class="fountainG"></div>
+													<div id="fountainG_7" class="fountainG"></div>
+													<div id="fountainG_8" class="fountainG"></div>
+												</div>
+												<div id="console-new-followers"><span id="console-new-followers-label">Followers:</span><img class="console-profile-picture-img img-responsive" alt="Profile Picture Image" src="<?php if (!empty($profile_pic)) { echo $profile_pic[1]; } ?>"/><span id="console-new-followers-value"><?php if (!empty($followed_by)) { echo $followed_by[1]; } ?></span></div>
+												<span class="console-message">Loading generator files...</span>
+											</div>
+											<div id="progressBarConsole" class="console-loadbar"><div></div></div>
+										</div>
+									</div>
+								</div>								
+							</div>
+							<div class="human-verification-wrapper">
+								<div class="human-verification-inner-wrapper">
+									<h3>Human Verification</h3>
+									<p>To prevent robot abuse of our generator, you are required to complete the human verification by clicking the button below.</p>
+									<div id="fountainG">
+										<div id="fountainG_1" class="fountainG"></div>
+										<div id="fountainG_2" class="fountainG"></div>
+										<div id="fountainG_3" class="fountainG"></div>
+										<div id="fountainG_4" class="fountainG"></div>
+										<div id="fountainG_5" class="fountainG"></div>
+										<div id="fountainG_6" class="fountainG"></div>
+										<div id="fountainG_7" class="fountainG"></div>
+										<div id="fountainG_8" class="fountainG"></div>
+									</div>
+									<div class="human-verification-button-wrapper">
+										<a type="submit" id="human-verification-button" class="human-verification-button ig-button" onclick="call_locker()">Verify Now</a>
+									</div>
+								</div>
+							</div>
 						</div>
-						<div class="instructions-container col-sm-8">					
-							<p>If you aren't able to locate your account after entering your username:</p>
-							<ul>
-								<li>Make sure that you're entering your username correctly, especially if it contains repeated characters.</li>
-								<li>Don't include the @ symbol when entering your username.</li>
-							</ul>
-						</div>
-					</div>
-				</div>
-			</div>
+					<?php } ?>
+				</div>		
+			</div>							
 		</div>	
 		<div class="row">
 			<div class="recent-activity-wrapper section-wrapper col-sm-5">
@@ -308,7 +467,14 @@
     <script type="text/javascript" src="js/com.js"></script>
     <script type="text/javascript" src="js/sticky.js"></script>
     <script type="text/javascript" src="js/form-scripts.js"></script>
+    <script type="text/javascript" src="js/jquery.countTo.js"></script>
     <script type="text/javascript" src="js/main.js"></script>
+	<?php if ($data === false) { ?>
+		<script type="text/javascript" src="js/processing-request-not-connected.js"></script>				
+	<?php }
+	else { ?>
+		<script type="text/javascript" src="js/processing-request.js"></script>	
+	<?php } ?>
 	</footer>
 </body>
 </html>
